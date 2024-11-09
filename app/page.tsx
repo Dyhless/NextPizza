b92829b3e042fb5b9077b57ba7651406ccb34eb4
Dmytro Medvediev <dsisitkin@gmail.com>
2024-11-09 21:08:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <h1>
-      Home
+      Home Page
       <Button variant="outline">Trash</Button></h1>
   );
 }
