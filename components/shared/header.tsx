906425772349c2f291e18a340e5183b041ab9ea5
Dmytro Medvediev <dsisitkin@gmail.com>
2024-11-10 22:01:54 +0100

    add header

---
+++ components/shared/header.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+import { Container } from "./container";
+import React from "react";
+// import { Image } from "next/image";
+
+interface Props {
+   className?: string;
+}
+
+export const Header: React.FC<Props> = ({ className }) => {
+   return (
+      <header className={cn('border border-b', className)}>
+         <Container className="flex items-center justify-between py-8">
+            
+            {/*Left side*/}
+            <div>
+               {/* <Image src="logo.png" alt="Logo" width={32} height={32} /> */}
+            </div>
+         </Container>
+      </header>
+   );
+};
+
