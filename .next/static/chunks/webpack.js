5c1df8c8cf8be58ba0e98095386a21e072ec02f8
Dmytro Medvediev <dsisitkin@gmail.com>
2024-12-19 14:14:30 +0100

    update selector of pizza

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f0bd501adcc28cf5"; }
+/******/ 		__webpack_require__.h = function() { return "c439e1de0afb5b0e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,3 +1,25 @@
-export default function ProductPage({ params: { id } }: { params: {id: string}}) {
-   return <p>Product {id}</p>
+import { Container, ProductImage, Title } from "@/components/shared";
+import { prisma } from "@/prisma/prisma-client";
+import { notFound } from 'next/navigation';
+
+
+export default async function ProductPage({ params: { id } }: { params: { id: string } }) {
+   const product = await prisma.product.findFirst({ where: { id: Number(id) } });
+
+   if (!product) {
+      return notFound();
+   }
+
+   return <Container className="flex flex-col my-10">
+      <div className="flex flex-col my-10">
+         <ProductImage imageUrl={product.imageUrl} size={ 40 } />
+      </div>
+
+      <div className="w-[490px] bg-[#F7F6F5] p-7">
+         <Title text={product.name} size="md" className="font-extrabold mb-1" />
+         
+         <p className="text-gray-400">Lorem ipsum dolor sit amet consectetur</p>
+      </div>
+
+   </Container>
 }
